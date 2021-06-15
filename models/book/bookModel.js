9627790fdcdb4aeccfa6250e7c799acb00e17d18
Tom <tmartinezrivero@alumno.unlam.edu.ar>
2021-06-15 13:05:06 -0300

    /book - establecer peticiones a la base de datos
    se crean funciones en el model de book para otener una lista de todos los libros, para obtener una lista de liros filtrados por campo y otener un libro por su id

--- models/book/bookModel.js
+++ models/book/bookModel.js
@@ -3,11 +3,26 @@ const connectDatabase = require("../../db")
 const db = connectDatabase();
 const qy = db.query
 
+async function getBookList(){
+    return await qy('SELECT * FROM `book`');
+}
+
+async function getBookById(id){
+    return await qy('SELECT * FROM `book` WHERE id = ?', [id]);
+}
+
+async function getBookListByField(field, value){
+    return await qy(`SELECT * FROM \`book\` WHERE ${field} = ?`, [ value ]);
+}
+
 async function newBook(setParams){
     return await qy('INSERT INTO `book` SET ?', [ setParams ] );
 }
 
 
 module.exports = {
-    newBook
+    newBook,
+    getBookList,
+    getBookById,
+    getBookListByField
 }
